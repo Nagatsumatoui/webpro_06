0fa87001d431478027964daddd2fe3181ae7f93d
Nagatsumatoui <nagatsumato@gmail.com>
2024-11-17 23:38:47 +0900

    コメント

--- README.md
+++ README.md
@@ -24,3 +24,24 @@ if -->|yes| win
 win --> end1
 if -->|nol| loose
 ```
+
+ファイル | 説明
+-|-
+app5.js | プログラム本体
+public/taipu.html | ポケモンのタイプ開始画面
+views/taipu.ejs | ポケモンのタイプテンプレートファイル
+
+```mermaid
+flowchart TD;
+
+start["開始"];
+end1["終了"]
+if{"条件に合うか"}
+win["効果抜群"]
+loose["いまひとつ"]
+
+start --> if
+if -->|yes| win
+win --> end1
+if -->|nol| loose
+```
--- app5.js
+++ app5.js
@@ -27,30 +27,89 @@ app.get("/luck", (req, res) => {
   res.render( 'luck', {number:num, luck:luck} );
 });
 
-app.get("/janken", (req, res) => {
+
+app.get("/taipu", (req, res) => {
   let hand = req.query.hand;
   let win = Number( req.query.win );
   let total = Number( req.query.total );
   console.log( {hand, win, total});
-  const num = Math.floor( Math.random() * 3 + 1 );
+  const num = Math.floor( Math.random() * 18 + 1 );
   let cpu = '';
-  if( num==1 ) cpu = 'グー';
-  else if( num==2 ) cpu = 'チョキ';
-  else cpu = 'パー';
+  if( num==1 ) cpu = 'ノーマル';
+  else if( num==2 ) cpu = '水';
+  else if( num==3 ) cpu = '草';
+  else if( num==4 ) cpu = '火';
+  else if( num==5 ) cpu = '電気';
+  else if( num==6 ) cpu = '地面';
+  else if( num==7 ) cpu = '氷';
+  else if( num==8 ) cpu = '格闘';
+  else if( num==9 ) cpu = '毒';
+  else if( num==10 ) cpu = '飛行';
+  else if( num==11 ) cpu = 'エスパー';
+  else if( num==12 ) cpu = '虫';
+  else if( num==13 ) cpu = '岩';
+  else if( num==14 ) cpu = 'ゴースト';
+  else if( num==15 ) cpu = 'ドラゴン';
+  else if( num==16 ) cpu = 'アク';
+  else if( num==17 ) cpu = '鋼';
+  else if( num==18 ) cpu = 'フェアリー';
   // ここに勝敗の判定を入れる
   let judgement = ''; 
   if (hand === cpu) { 
-      judgement = 'あいこ';
+      judgement = '通常';
   } else if ( 
-    (hand === 'グー' && cpu === 'チョキ') ||
-    (hand === 'チョキ' && cpu === 'パー') ||
-    (hand === 'パー' && cpu === 'グー') 
+    (hand === '水' && cpu === '火') ||
+    (hand === '水' && cpu === '地面') ||
+    (hand === '水' && cpu === '岩') ||
+    (hand === '草' && cpu === '水') ||
+    (hand === '草' && cpu === '地面') ||
+    (hand === '草' && cpu === '岩') ||
+    (hand === '火' && cpu === '草') ||
+    (hand === '火' && cpu === '虫') ||
+    (hand === '火' && cpu === '氷') ||
+    (hand === '火' && cpu === '鋼') ||
+    (hand === '電気' && cpu === '水') ||
+    (hand === '電気' && cpu === '飛行') ||
+    (hand === '地面' && cpu === '火') ||
+    (hand === '地面' && cpu === '電気') ||
+    (hand === '地面' && cpu === '毒') ||
+    (hand === '地面' && cpu === '岩') ||
+    (hand === '地面' && cpu === '鋼') ||
+    (hand === '氷' && cpu === '草') ||
+    (hand === '氷' && cpu === '地面') ||
+    (hand === '氷' && cpu === '飛行') ||
+    (hand === '氷' && cpu === 'ドラゴン') ||
+    (hand === '格闘' && cpu === 'ノーマル') ||
+    (hand === '毒' && cpu === '草') ||
+    (hand === '毒' && cpu === 'フェアリー') ||
+    (hand === '飛行' && cpu === '格闘') ||
+    (hand === '飛行' && cpu === '虫') ||
+    (hand === '飛行' && cpu === '草') ||
+    (hand === 'エスパー' && cpu === '格闘') ||
+    (hand === 'エスパー' && cpu === '毒') ||
+    (hand === '虫' && cpu === '草') ||
+    (hand === '虫' && cpu === 'エスパー') ||
+    (hand === '虫' && cpu === 'アク') ||
+    (hand === '岩' && cpu === '火') ||
+    (hand === '岩' && cpu === '氷') ||
+    (hand === '岩' && cpu === '飛行') ||
+    (hand === '岩' && cpu === '虫') ||
+    (hand === 'ゴースト' && cpu === 'ゴースト') ||
+    (hand === 'ドラゴン' && cpu === 'ドラゴン') ||
+    (hand === 'アク' && cpu === 'エスパー') ||
+    (hand === 'アク' && cpu === 'ゴースト') ||
+    (hand === '鋼' && cpu === '氷') ||
+    (hand === '鋼' && cpu === '岩') ||
+    (hand === '鋼' && cpu === 'フェアリー') ||
+    (hand === 'フェアリー' && cpu === '格闘') ||
+    (hand === 'フェアリー' && cpu === 'アク') ||
+    (hand === 'フェアリー' && cpu === 'ドラゴン') 
   ) {
   // 今はダミーで人間の勝ちにしておく
-  judgement = '勝ち';
+  judgement = '効果抜群';
   win += 1;
 } else {
-  judgement = '負け';
+  judgement = 'いまひとつ';
 }
   total += 1;
   const display = {
@@ -60,7 +119,8 @@ app.get("/janken", (req, res) => {
     win: win,
     total: total
   };
-  res.render( 'janken', display );
+  res.render( 'taipu', display );
 });
+
 
 app.listen(8080, () => console.log("Example app listening on port 8080!"));
